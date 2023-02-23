6b979befa12c99a874ed9ebb2b14c23a1bcb4b5d
csabbah <csabbah@ryerson.ca>
2023-02-22 19:24:53 -0500

    Updated assets folder & added fonts to be used in App.js

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+
 import { useFonts } from "expo-font";
+
 import Details from "./screens/Details";
 import Home from "./screens/Home";
 
@@ -16,6 +18,17 @@ const theme = {
 };
 
 const App = () => {
+  // Load up all fonts to be used in project
+  const [loaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    interLight: require("./assets/fonts/Inter-Light.ttf"),
+  });
+  // If fonts are not loaded, don't load app
+  if (!loaded) return null;
+
   return (
     <NavigationContainer theme={theme}>
       <Stack.Navigator
